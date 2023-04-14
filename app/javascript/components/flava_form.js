ace215bd991cc31138caac1b994e742e07e17ee1
chrisdrydenaltmetric <chris@altmetric.com>
2023-04-14 16:20:37 +0100

    Rename app to hackday project

--- app/javascript/components/flava_form.js
+++ app/javascript/components/flava_form.js
@@ -21,19 +21,18 @@ export default class FlavaForm extends Component {
 
 	render() {
 		return html`
-            <form onSubmit=${(e) => this.handleSubmit(e)}>
-                <label>
-                    Pick your favorite flavor:
-                    <select value=${this.state.value} onChange=${(e) => this.handleChange(e)}>
-                        <option value="grapefruit">Grapefruit</option>
-                        <option value="lime">Lime</option>
-                        <option value="coconut">Coconut</option>
-                        <option value="mango">Mango</option>
-                    </select>
-                </label>
-                <input type="submit" value="Submit" />
-            </form>
-        `
-			;
+      <form onSubmit=${(e) => this.handleSubmit(e)}>
+        <label>
+          Pick your favorite flavor:
+          <select value=${this.state.value} onChange=${(e) => this.handleChange(e)}>
+            <option value="grapefruit">Grapefruit</option>
+            <option value="lime">Lime</option>
+            <option value="coconut">Coconut</option>
+            <option value="mango">Mango</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    `;
 	}
 }
